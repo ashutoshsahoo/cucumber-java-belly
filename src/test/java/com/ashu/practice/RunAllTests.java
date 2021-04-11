4235cc6da4a8d1da1e1cbbfcf6330b666db2ae16
ashutoshsahoo <15648046+ashutoshsahoo@users.noreply.github.com>
2021-04-11 16:39:00 +0530

    update config

--- src/test/java/com/ashu/practice/RunAllTests.java
+++ src/test/java/com/ashu/practice/RunAllTests.java
@@ -6,9 +6,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        plugin = {"pretty", "html:target/cucumber-report.html"},
-        features = "src/test/resources/features"
-)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber-report.html"})
 public class RunAllTests {
 }
